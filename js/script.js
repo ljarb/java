67a159774273ca8cf2cdacb8b9d04da306fd417d
jarbi luna <jarbiluna@gmail.com>
2026-05-07 17:45:02 -0400

    act

--- js/script.js
+++ js/script.js
@@ -3,7 +3,8 @@ let opcion = Number(prompt(
     "1. validar si es mayor de edad"+
     "2. validar si un numero es positivo , negativo y cero" +
     "3.evaluar notas"+
-    "4.dia de la semana"
+    "4.dia de la semana"+
+    "tabal de multiplicar"
 ));
 if (opcion === 1){
     let edad= Number(prompt("ingrese su edad"))
@@ -59,6 +60,12 @@ if (opcion === 1){
         default:
             alert("numero no valido, debe ingresar entre dia de la semana del 1 al 7")
     }
+} else if (opcion ===5){
+    let num=Number(prompt("ingrese un numero para generar su tabla:"))
+    console.log(`tabla de multiplicar ${num}`);
+    for (let i =1; i<= 10; i++)(
+        console.log(`${num} x ${i}=${num*1}`)
+    )
 }
 else{
     alert("opcion no valida debe seleccionar entre el 1 al 9")
